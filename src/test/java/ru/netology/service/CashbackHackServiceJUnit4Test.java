506f8fb025d29255056e5a0a51868e4a7b794d96
Evgenia Larina <larsik29@yandex.ru>
2024-09-24 21:19:38 +0300

    change commit

--- src/test/java/ru/netology/service/CashbackHackServiceJUnit4Test.java
+++ src/test/java/ru/netology/service/CashbackHackServiceJUnit4Test.java
@@ -1,2 +1,45 @@
-package ru.netology.service;public class CashbackHackServiceJUnit4Test {
+package ru.netology.service;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CashbackHackServiceJUnit4Test {
+
+    @Test
+    public void testRemainUnderBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+        int expected = 100;
+        int actual = service.remain(amount);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testRemainOnBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int expected = 1000;
+        int actual = service.remain(amount);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testRemainOverBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1100;
+        int expected = 900;
+        int actual = service.remain(amount);
+        Assert.assertEquals(expected, actual);
+    }
+
+    // Этот тест должен падать, так как в сервисе есть ошибка
+    @Test
+    public void testRemainExactMultipleOfBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 2000;
+        int expected = 0;
+        int actual = service.remain(amount);
+        Assert.assertEquals(expected, actual);  // Ожидается 0, но получим 1000
+    }
 }
+
